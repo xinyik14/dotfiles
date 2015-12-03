d5364cbec3858f5d0a40bc526b18bfbeac8914c7
Jian Shen <shenj.soso@gmail.com>
2015-12-02 22:33:35 -0800

    files updated

--- sync.sh
+++ sync.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 PWD=$(pwd)
+cp $HOME/.tmuxinator $PWD/
 cp $HOME/.profile $PWD/
 cp $HOME/.vimrc $PWD/
+cp $HOME/.vim $PWD/
 cp $HOME/.bashrc $PWD/
 cp $HOME/.gitconfig $PWD/
 echo "last sync at $(date)" > $PWD/sync.succeed
