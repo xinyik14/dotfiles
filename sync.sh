caa184136b9532f5f2dd0559e73b636182e561a7
Jian Shen <shenj.soso@gmail.com>
2015-12-03 00:26:02 -0800

    No need to sync .vim

--- sync.sh
+++ sync.sh
@@ -4,7 +4,6 @@ PWD=$(pwd)
 cp -r $HOME/.tmuxinator $PWD/
 cp $HOME/.profile $PWD/
 cp $HOME/.vimrc $PWD/
-cp -r $HOME/.vim $PWD/
 cp $HOME/.bashrc $PWD/
 cp $HOME/.gitconfig $PWD/
 echo "last sync at $(date)" > $PWD/sync.succeed
